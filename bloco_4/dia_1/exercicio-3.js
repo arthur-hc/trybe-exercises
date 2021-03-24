723807c01320433bbcdec139489354215eb0fa37
Arthur Hermann <arthurhermannc@gmail.com>
2021-03-24 20:32:58 -0300

    exercicio 3

---
+++ bloco_4/dia_1/exercicio-3.js
@@ -0,0 +1,13 @@
+let n1 = 20
+let n2 = 25
+let n3 = 13
+
+if (n1>n2 && n1>n3) {
+    console.log (n1);
+} else if (n2>n1 && n2>n3) {
+    console.log (n2);
+} else if (n3>n1 && n3>n2) {
+    console.log (n3);
+} else {
+    console.log ('Não há um número maior')
+}
