44d66866073bc3f76e4d7e7ffd4d9c058c3d257c
Arthur Hermann <arthurhermannc@gmail.com>
2021-03-25 18:43:39 -0300

    exercicio 2

---
+++ bloco_4/dia_2/exercicio2.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let somatoria = 0;
+for (let index =0; index <numbers.length; index +=1) {
+    somatoria = somatoria + numbers[index]
+}
+
+console.log (somatoria)
+
+//Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;//
