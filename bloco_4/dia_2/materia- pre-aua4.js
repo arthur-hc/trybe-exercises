2ba62ec8284a4e6fb91e4468293993d3a1110113
Arthur Hermann <arthurhermannc@gmail.com>
2021-03-25 16:22:23 -0300

    exercicio 4

---
+++ bloco_4/dia_2/materia- pre-aua4.js
@@ -0,0 +1,18 @@
+let numero = 7
+for (index=1; index<=9; index +=1) {
+    console.log (numero*index)
+}
+// Mostrará a multiplicação do 7, de 1 até 9 //
+
+let listaDeNomes = ['Joana', 'Maria', 'Lucas'];
+
+for (let index2 = 0; index2 < listaDeNomes.length; index2 +=1) {
+    console.log ('Boas vindas ' + listaDeNomes[index2] +'!')
+}
+// Dará boas vindas a cada pessoa contida no array //
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for(let index3 =0; index3 < cars.length; index3 +=1) {
+    console.log (cars[index3])
+}
