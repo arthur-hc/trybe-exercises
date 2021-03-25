794bd52005c64fe8fd22f695747c90d5b33f8cf8
Arthur Hermann <arthurhermannc@gmail.com>
2021-03-25 12:59:30 -0300

    exercicio 9

--- bloco_4/dia_1/exercicio-8.js
+++ bloco_4/dia_1/exercicio-8.js
@@ -1,11 +1,11 @@
-let n1 =12
-let n2 =13
-let n3 =10
+let n1 =13
+let n2 =12
+let n3 =13
 let result
 
-if (n1%2>0 || n2%2>0 || n3%2>0) {
+if (n1%2===0 || n2%2===0 || n3%2===0) {
     result = true
 } else {
     result = false
 }
-console.log ("Pelo menos um é ímpar? " + result)
+console.log ("Pelo menos um é par? " + result)
---
+++ bloco_4/dia_1/exercicio-9.js
@@ -0,0 +1,11 @@
+let n1 =12
+let n2 =13
+let n3 =10
+let result
+
+if (n1%2>0 || n2%2>0 || n3%2>0) {
+    result = true
+} else {
+    result = false
+}
+console.log ("Pelo menos um é ímpar? " + result)
