7a165246de7cd39bab693cce439df138721642ce
Arthur Hermann <arthurhermannc@gmail.com>
2021-03-25 19:01:07 -0300

    exercicio 3

---
+++ bloco_4/dia_2/exercicio3.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let media = 0
+for (let index; index < numbers.length; index +=1){
+    media = media + (numbers [index]/numbers.length)
+}
+
+console.log (media)
+
+
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;//
