9de26d8b8a87d16cf53a3e3e39905967c033bbd1
Arthur Hermann <arthurhermannc@gmail.com>
2021-03-25 13:11:23 -0300

    tirando switch do exe-7

--- bloco_4/dia_1/exercicio-7.js
+++ bloco_4/dia_1/exercicio-7.js
@@ -1,30 +1,5 @@
 let nota = 90
 
-switch (nota) {
-  case nota >=90:
-    console.log ('A')
-    break;
-  
-  case nota >=80:
-    console.log ('B')
-    break;
-  
-  case nota >=70:
-    console.log ('C')
-    break;
-  
-  case nota >=60:
-    console.log ('D')
-    break;
-  
-  case nota >=50:
-    console.log ('E')
-    break;
-        
-  default:
-    console.log ('F')
-}
-
 if (nota>100 || nota<0)
   console.log('Erro')
   else if(nota >=90) {
