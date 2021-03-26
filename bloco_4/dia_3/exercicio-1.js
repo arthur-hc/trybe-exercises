c2c1934723bb6a331efc3c735f24a745404bdeae
Arthur Hermann <arthurhermannc@gmail.com>
2021-03-26 19:09:00 -0300

    exercicio 1

---
+++ bloco_4/dia_3/exercicio-1.js
@@ -0,0 +1,11 @@
+n = 5
+ let asteriscos = '*'
+ let linhaAsteriscos = ''
+
+ for (let index = 1; index <= n; index +=1) {
+    linhaAsteriscos += asteriscos
+ }
+ 
+ for(let index = 1; index <= n; index +=1) {
+     console.log (linhaAsteriscos)
+ }
