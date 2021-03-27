40c7cc775f5def545c22489b720e960f7dd0ee91
Arthur Hermann <arthurhermannc@gmail.com>
2021-03-27 17:19:01 -0300

    exercicios com adicicao de comentarios e algumas correcoes

--- bloco_4/dia_1/exe-pre-aula2.js
+++ bloco_4/dia_1/exe-pre-aula2.js
@@ -5,3 +5,11 @@ let birthYear = 1995
 console.log (firstName)
 console.log (birthCity)
 console.log (birthYear)
+
+//Crie uma constante chamada name e atribua a ela o seu nome; 
+//Crie uma constante chamada birthCity e atribua a ela a sua cidade natal; 
+//Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu; 
+//Utilize o console.log() para imprimir as constantes e variáveis que você criou; 
+//Altere o valor atribuído à variável birthYear para 2020. 
+//Faça um console.log(birthYear) novamente para ver o que acontece!; Altere o valor atribuído à constante birthCity. 
+//Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro?//
