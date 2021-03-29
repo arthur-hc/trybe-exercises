7d7af45d0adfc3fa269fe03f4c0658f9cb2c8b12
Arthur Hermann <arthurhermannc@gmail.com>
2021-03-29 18:49:38 -0300

    exercicio pre aula 3

---
+++ bloco_4/dia_4/for-in-of.js
@@ -0,0 +1,7 @@
+let food = ['hamburguer', 'bife', 'acarajé'];
+
+for (let index of food) {
+    console.log(index);
+  }
+  //resultado (in): 0, 1, 2;
+  // resultado (of): hamburguer, bife, acarajé
