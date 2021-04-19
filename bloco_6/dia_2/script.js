9820edcfc7638cf9b12fe7e0f680174bc04aaae2
Arthur Hermann <arthurhermannc@gmail.com>
2021-04-19 14:52:49 -0300

    adiciona bibiliotecas

--- bloco_6/dia_2/script.js
+++ bloco_6/dia_2/script.js
@@ -1,5 +1,5 @@
 const selectStates = document.querySelector('#select-state');
-const states = ['AC',  'AL',  'AP',  'AM',  'BA',  'CE',  'DF',  'ES',  'GO',  'MA',  'MT',  'MS',  'MG',  'PA',  'PB',  'PR',  'PE',  'PI',  'RJ',  'RN',  'RS',  'RO',  'RR',  'SC',  'SP',  'SE',  'TO'];
+const states = ['Selecione o Estado', 'AC',  'AL',  'AP',  'AM',  'BA',  'CE',  'DF',  'ES',  'GO',  'MA',  'MT',  'MS',  'MG',  'PA',  'PB',  'PR',  'PE',  'PI',  'RJ',  'RN',  'RS',  'RO',  'RR',  'SC',  'SP',  'SE',  'TO'];
 const preventDefault = document.querySelector('#default');
 const dataUser = document.querySelector('#form-data');
 const submitButton = document.querySelector('#button-submit');
@@ -32,9 +32,6 @@ function renderCurriculum(event) {
   const formElements = document.querySelectorAll('input');
   if (checkEmail()) {
     for (let index = 0; index < formElements.length; index += 1) {
-      if (formElements[index].type === 'radio' && !formElements[index].checked) {
-        continue;
-      }
       const userInput = formElements[index].value;
         const div = document.createElement('div');
         div.className = 'div-curriculum';
@@ -58,3 +55,24 @@ deleteButton.addEventListener('click', deleteResume)
 window.onload = function() {
   insertStates()
 }
+
+let picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  format: 'D/M/YYYY',
+  toString(date, format) {
+      // you should do formatting based on the passed format,
+      // but we will just return 'D/M/YYYY' for simplicity
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+      // dateString is the result of `toString` method
+      const parts = dateString.split('/');
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1;
+      const year = parseInt(parts[2], 10);
+      return new Date(year, month, day);
+  }
+});
