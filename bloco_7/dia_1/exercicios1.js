7edeabe8890492d7b01d1d7efbf4b5a8d1d07103
Arthur Hermann <arthurhermannc@gmail.com>
2021-04-26 19:35:06 -0300

    exercicio 1.2

--- bloco_7/dia_1/exercicios1.js
+++ bloco_7/dia_1/exercicios1.js
@@ -19,10 +19,20 @@ const testingScope = escopo => {
     if (escopo === true) {
         let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
         ifScope = `${ifScope} ' ótimo, fui utilizada no escopo !'`
-        console.log (ifScope)
+        console.log (ifScope);
     } else {
         let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
-        console.log (elseScope)
+        console.log (elseScope);
     }
 }
-testingScope(false)
+testingScope(false);
+// 
+// 
+// Criar função com template literals para devolver o array em ordem crescente
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const sort = () => {
+   return oddsAndEvens.sort(function(a, b){return a-b})
+}
+// Seu código aqui.
+
+console.log(`Os números ${sort()} se encontram ordenados de forma crescente !`);
