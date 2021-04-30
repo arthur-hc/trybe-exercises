77e1414bcccfd2b3ea3d268215e82c520da69818
Arthur Hermann <arthurhermannc@gmail.com>
2021-04-30 18:37:06 -0300

    bonus 1

--- bloco_8/dia_1/exercicio.js
+++ bloco_8/dia_1/exercicio.js
@@ -1,8 +1,10 @@
+// Retorna um objeto contendo nome e email gerado
 const addEmployeeInfo = (name) => {
   let email = `${name.toLowerCase().replace(/\s/gi, '_')}@trybe.com`;
   return {name, email};
 };
 
+//Retorna um objeto com id nomes e email adicionados através da função anterior, utilizando como parâmetro os nomes no objeto employees
 const newEmployees = (block) => {
   const employess = {
     id1: block('Arthur Hermann'),
@@ -14,10 +16,12 @@ const newEmployees = (block) => {
 
 console.log(newEmployees(addEmployeeInfo));
 
+//Retorna checagem de dois números
 const checkBet = (num1,num2) => {
   return num1 === num2
 };
 
+//Retorna uma mensagem de acordo com o resultado gerado e aposta feita passada como parâmetro, e utiliza a função anterior passando como parâmetro para checar a equidade dos valores
 const betResult = (yourBet, block) => {
   const result = Math.floor(Math.random()*6)
   if (block(result,yourBet)) {
