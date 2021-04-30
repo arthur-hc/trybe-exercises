195c189760de71ccb82663c2d623f13173f23e6f
Arthur Hermann <arthurhermannc@gmail.com>
2021-04-30 18:19:20 -0300

    1.2

--- bloco_8/dia_1/exercicio.js
+++ bloco_8/dia_1/exercicio.js
@@ -3,13 +3,28 @@ const addEmployeeInfo = (name) => {
   return {name, email};
 };
 
-const newEmployees = (callback) => {
+const newEmployees = (block) => {
   const employess = {
-    id1: callback('Arthur Hermann'),
-    id2: callback('Lucas Gil'),
-    id3: callback('Igor Akio'),
+    id1: block('Arthur Hermann'),
+    id2: block('Lucas Gil'),
+    id3: block('Igor Akio'),
   }
   return employess
 };
 
 console.log(newEmployees(addEmployeeInfo));
+
+const checkBet = (num1,num2) => {
+  return num1 === num2
+};
+
+const betResult = (yourBet, block) => {
+  const result = Math.floor(Math.random()*6)
+  if (block(result,yourBet)) {
+    return 'Parabéns, você ganhou!'
+  } else {
+    return 'Tente novamente'
+  }
+}
+
+console.log(betResult(4,checkBet));
