79889c1ebe527ef5ad82b288252e3f06bdceb362
Arthur Hermann <arthurhermannc@gmail.com>
2021-05-28 21:53:44 -0300

    resumo

--- 2 - Desenvolvimento Front-end/Bloco_12 (Componentes com Estado, Eventos e Forms)/dia_2 (Forms no React)/Resumo.js
+++ 2 - Desenvolvimento Front-end/Bloco_12 (Componentes com Estado, Eventos e Forms)/dia_2 (Forms no React)/Resumo.js
@@ -35,3 +35,92 @@ class Form extends Component {
 }
  
 export default Form;
+
+// CRIANDO A FUNÇÃO DE FORMA GENÉRICA
+// PRIMEIRO, DESESTRUTURA O EVENT.TARGET => { TARGET } 
+handleChange({ target }) {
+  // IRÁ UTILIZAR O NAME DO CAMPO(ex: estadoFavorito PS: É o que está sendo indicado dentro do textarea)
+  const { name } = target;
+  // IRÁ ARMAZENAR O VALUE
+  // CASO SEJA DO TIPO CHECKBOX O CAMINHO É TARGET.CHECKED : SE NÃO O CAMINHO É TARGET.VALUE
+  // NA CONST VALUE, SERÁ ARMAZENADO O VALOR OBTIDO ATRAVÉS DE UM DOS CAMINHOS
+  const value = target.type === 'checkbox' ? target.checked : target.value;
+
+  // COMO O NAME NO CAMPO É O MESMO QUE ESTA NO STATE, É POSSÍVEL FAZER A INTERPOLAÇÃO
+  // OU SEJA NO CASO DO Estado Favorito o name do campo será setado no stade, entretanto, como ele já possui uma key com esse mesmo nome, não haverá diferença. Apenas será dada um novo valor que será o que foi armazenado em value. 
+  this.setState({
+    [name]: value,
+  });
+}
+
+// FUNÇÃO SEM COMENTÁRIOS
+handleChange({ target }) {
+  const { name } = target;
+  const value = target.type === 'checkbox'? target.checked : target.value;
+  this.setState({
+    [name]: value,
+  })
+}
+
+// PARA SEPARAR OS COMPONENTES FILHOS DO PAI E TRANSMITIR DADOS ENTRE ELES...
+
+// App.js
+import React, { Component } from 'react';
+import EstadoFavorito from './EstadoFavorito'
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      estadoFavorito: '',
+      nome: '',
+      email: '',
+      idade: 0,
+    }
+  }
+  handleChange({ target }) {
+    const { name } = target
+    const value = target.type === 'checkbox'? target.checked : target.value
+    this.setState({ 
+      [name]: value 
+    })
+  }
+
+  render() { 
+    return ( 
+      <div>
+        <h1>NÃO IRÁ RENDERIZAR, APENAS UM EXEMPLO</h1>
+        <form>
+          {/* Agora o componente é chamado e o valor do estadoFav e a Func são passados via props */}
+          <EstadoFavorito value={this.state.estadoFavorito} handleChange={this.handleChange}/>
+        </form>
+      </div>
+    );
+  }
+}
+ 
+export default Form;
+
+// NO EstadoFavorito.js
+import React, { Component } from 'react';
+class EstadoFavorito extends Component {
+  render() {
+    // o value e a func são resgatadas via props e utilizadas no componente que será devolvido para o elemento pai
+    const { value, handleChange } = this.props
+    return (
+      <label>
+        Estado Favorito
+        <textarea 
+        name="estadoFavorito" 
+        // O valor alterado aqui, continuará sendo direcionado para o state
+        value={value} 
+        // Com alterações, continua sendo dispara do a func
+        onChange={ handleChange }
+        />
+      </label>
+    );
+  }
+}
+export default EstadoFavorito;
+
