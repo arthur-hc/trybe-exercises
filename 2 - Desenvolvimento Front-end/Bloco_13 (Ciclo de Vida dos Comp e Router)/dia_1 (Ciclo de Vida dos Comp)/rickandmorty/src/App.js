add0305e6e5337b19cc0dd530085a20e2f13c10c
Arthur Hermann <arthurhermannc@gmail.com>
2021-06-06 11:10:12 -0300

    termina pre aula

--- 2 - Desenvolvimento Front-end/Bloco_13 (Ciclo de Vida dos Comp e Router)/dia_1 (Ciclo de Vida dos Comp)/rickandmorty/src/App.js
+++ 2 - Desenvolvimento Front-end/Bloco_13 (Ciclo de Vida dos Comp e Router)/dia_1 (Ciclo de Vida dos Comp)/rickandmorty/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { Component }from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        characters: [],
+    };
+  }
+
+
+  //COM .THEN()
+  // fetchCharacters = () => {
+  //   fetch('https://rickandmortyapi.com/api/character')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     this.setState({characters: data.results})
+  //   })
+  // }
+
+  //COM ASYNC
+  async fetchCharacters() {
+    const requestApi = await fetch('https://rickandmortyapi.com/api/character')
+    const objtJson = await requestApi.json();
+    this.setState({characters: objtJson.results})
+  }
+
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
+  // //OUTRA MANEIRA
+  // componentDidMount() {
+  //   fetch('https://rickandmortyapi.com/api/character')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     this.setState({characters: data.results})
+  //   })
+  // }
+
+    render() {
+    const { characters } = this.state
+    return (
+      <div className="App">
+        <h1>
+          Ricky and Morty Characters:
+        </h1>
+        <div className="body">
+          {characters.map(({name, image}) => {
+            return (
+              <div className="container" key={name}>
+                <h3>{name}</h3>
+                <img src={image} alt={name}/>
+              </div>
+            )
+          })}
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
