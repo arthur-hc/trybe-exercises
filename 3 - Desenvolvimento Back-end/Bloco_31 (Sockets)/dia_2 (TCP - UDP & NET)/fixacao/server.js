c9abe9a395e75b39eb820b01b7349243199b2a40
Arthur Hermann <arthurhermannc@gmail.com>
2021-12-06 19:34:13 -0300

    add lecture example

--- 3 - Desenvolvimento Back-end/Bloco_31 (Sockets)/dia_2 (TCP - UDP & NET)/fixacao/server.js
+++ 3 - Desenvolvimento Back-end/Bloco_31 (Sockets)/dia_2 (TCP - UDP & NET)/fixacao/server.js
@@ -1,17 +1,61 @@
-/* Importando o pacote NET, responsável pela implementação dos sockets no Node. */
+// /* Importando o pacote NET, responsável pela implementação dos sockets no Node. */
+// const net = require('net');
+
+// /* Criando o servidor com o método 'createServer', onde recebe uma conexao na qual são expostos os eventos que podemos manipular no nosso servidor. */
+// const server = net.createServer((connection) => {
+//   console.log('Cliente conectado');
+
+//   /* Assim como um evento normal do Node.js, o método ".on()" escuta um evento em específico e, quando ele é ativado, nossa função de callback é chamada. */
+//   connection.on('end', () => {
+//     console.log('Cliente desconectado');
+//   });
+//   /* Nessa conexão que foi aberta, podemos fazer várias coisas. Uma delas é escrever/devolver uma mensagem para o cliente. */
+//   connection.write('Mensagem do servidor!\r\n');
+//   connection.pipe(connection);
+// });
+
+// /* Após termos programado o servidor, é só colocá-lo de pé */
+// server.listen(8080, () => {
+//   console.log('Servidor escutando na porta 8080');
+// });
+
+// EXEMPLO RENATO
+
+
 const net = require('net');
 
-/* Criando o servidor com o método 'createServer', onde recebe uma conexao na qual são expostos os eventos que podemos manipular no nosso servidor. */
-const server = net.createServer((connection) => {
-  console.log('Cliente conectado');
+const connectedClients = [];
+let clientId = 0;
+
+const broadCastMessage = (from, message) => {
+  connectedClients.forEach((client) => {
+    if (client.id !== from.id) {
+      client.write(message);
+    }
+  });
+}
+
+const server = net.createServer((socket) => {
+  console.log('Novo cliente conectado');
+
+  clientId = clientId + 1;
+  socket.id = `Cliente ${clientId}`,
+
+  socket.write(`Bem vindo, ${socket.id}!`);
+
+  connectedClients.push(socket);
+
+  broadCastMessage(socket, `O cliente #${socket.id} entrou!`);
+  
+  socket.on('data', (data) => {
+    broadCastMessage(socket, `cliente #${socket.id} > ${data}`);
+  });
 
   /* Assim como um evento normal do Node.js, o método ".on()" escuta um evento em específico e, quando ele é ativado, nossa função de callback é chamada. */
-  connection.on('end', () => {
-    console.log('Cliente desconectado');
+  socket.on('end', () => {
+    console.log(`O Cliente #${socket.id} saiu!`);
+    broadCastMessage(socket, `cliente #${socket.id} se desconectou`);
   });
-  /* Nessa conexão que foi aberta, podemos fazer várias coisas. Uma delas é escrever/devolver uma mensagem para o cliente. */
-  connection.write('Mensagem do servidor!\r\n');
-  connection.pipe(connection);
 });
 
 /* Após termos programado o servidor, é só colocá-lo de pé */
