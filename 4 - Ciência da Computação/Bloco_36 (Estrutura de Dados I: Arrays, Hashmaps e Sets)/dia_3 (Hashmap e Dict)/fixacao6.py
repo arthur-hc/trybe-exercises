2207df998078a46d319440c04d80420e75a9dd0e
Arthur Hermann <arthurhermannc@gmail.com>
2022-03-06 22:32:28 -0300

    pre content 36.3

---
+++ 4 - Ciência da Computação/Bloco_36 (Estrutura de Dados I: Arrays, Hashmaps e Sets)/dia_3 (Hashmap e Dict)/fixacao6.py
@@ -0,0 +1,27 @@
+# Exercício 6: Dada uma string, construa um dicionário com a contagem de
+# cada caractere da palavra. Utilize o pseudocódigo e o exemplo abaixo para
+# se nortear.
+
+# Para cada char na string:
+#     - Se o char não estiver no dicionário, inclua com o valor 1;
+
+#     - Se estiver, incremente o valor.
+
+
+# # Exemplo:
+
+# str = "bbbbaaaacccaaaaaaddddddddccccccc"
+# # saída: {'b': 4, 'a': 10, 'c': 10, 'd': 8}
+
+# str = "coxinha"
+# # saída: {'c': 1, 'o': 1, 'x': 1, 'i': 1, 'n': 1, 'h': 1, 'a': 1}
+# # Explicação: Nenhuma letra repete em coxinha :)
+
+from collections import Counter
+
+
+def repeated_counter(string):
+    return dict(Counter(string))
+
+
+print(repeated_counter("coxinha"))
